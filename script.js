a5b4be3f99161de8a1e97e262a7f2b5bd26dbf86
shivraj khating <67963572+shivrajkhating@users.noreply.github.com>
2025-11-15 14:09:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Select the button and dropdown
+const removeBtn = document.querySelector('input[type="button"]');
+const colorSelect = document.getElementById('colorSelect');
+
+removeBtn.addEventListener('click', () => {
+  // Get the selected index
+  const selectedIndex = colorSelect.selectedIndex;
+
+  // If a valid option is selected, remove it
+  if (selectedIndex !== -1) {
+    colorSelect.remove(selectedIndex);
+  }
+});
